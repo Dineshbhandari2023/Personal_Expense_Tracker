0e2915a54adc4eccd1aea1cdefc68aa33453b0a8
Dineshbhandari2023 <iamdinesh.github@gmail.com>
2025-01-01 20:44:35 +0545

    Working on UI

---
+++ Data/UserDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Models;
+
+public class UserDb : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public UserDb(DbContextOptions<UserDb> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=users.db");
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class User
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; } // This will store the hashed password
+        public required string Email { get; set; }
+
+    }
+}
